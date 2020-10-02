e65bc9b0082749a621d8a2b71b384d8645cd6b6c
kergeres <55786735+kergeres@users.noreply.github.com>
2020-10-02 14:57:51 +0200

    02e

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
      const elem = document.querySelector(item);
 
      elem.style.transform = `translateY(${distance * speed}px)`;
+     //  elem.style.transform = ` rotate(${distance * speed/2}deg)`;
  }
 
  window.addEventListener("scroll", function () {
-     parallax(".eleven", window.scrollY, 0.5);
-     parallax(".fourteen", window.scrollY, 0.4);
-     parallax(".sixteen", window.scrollY, 1.5);
+     parallax(".negyvenegy", window.scrollY, 0.8);
+     parallax(".negyven", window.scrollY, 0.3);
+     parallax(".het", window.scrollY, 0.4);
      parallax(".ten", window.scrollY, 0.2);
      parallax(".twelve", window.scrollY, 0.9);
      parallax(".thirteen", window.scrollY, 0.8);
@@ -15,4 +16,22 @@
 
  });
 
- console.log("sega");
+
+
+
+
+
+ //  function proba() {
+ //      let iti = document.querySelector(".hexagon");
+ //      let append = "";
+ //      for (let i = 0; i < 30; i++) {
+ //          append += `
+ //          ${iti}
+ //          `;
+ //          document.querySelector(".hexagn").innerHTML = append;
+ //      }
+
+
+ //  }
+
+ //  proba()
